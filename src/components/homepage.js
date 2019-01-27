c8c1a3c28124febbdc8c6a719d576794320c3826
Steven Imus <imussg@gmail.com>
2019-01-26 21:06:22 -0500

    added a few style changes; trying to get the look right first

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Grid, Row, Col, Breadcrumb, Image } from 'react-bootstrap';
+import { Grid, Row, Col, Breadcrumb, Image, Label, Glyphicon } from 'react-bootstrap';
 
 const thaipadLogo = require('../images/thaipad_logo1.png');
+const togoIcon = require('../images/togo-icon.png');
 
 export default class Homepage extends React.Component {
 	render() {
 		return ( <Grid>
-			<Row>
-				<Col>
-					<Breadcrumb>
+			<Row className="navbar-row">
+				<Breadcrumb>
+					<Col xs={2}>
 						<Breadcrumb.Item bsSize="large" href="#">
 							<Image className="" src={thaipadLogo} circle />
 						</Breadcrumb.Item>
-					</Breadcrumb>
-				</Col>
+					</Col>
+					<Col xs={2}>
+						<Breadcrumb.Item bsSize="large" href="#">
+							<h2>{'   Order to go   '}</h2>
+						</Breadcrumb.Item>
+					</Col>
+				</Breadcrumb>
 			</Row>
 		</Grid> );
 	}
