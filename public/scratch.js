cb2f5248b7bd314b1e44b267a4251287d6c65bc5
Steven Imus <imussg@gmail.com>
2019-02-08 19:04:05 -0500

    progress has been made css wise

--- public/scratch.js
+++ public/scratch.js
@@ -7,9 +7,24 @@ $(document).ready(function() {
 	// const sukhothai = require('../src/images/sukhothai.jpg');
 	// console.log(window.location.pathname);
 	// $('body').html(`<body style="background-image: url(${sukhothai});">`);
-	// $('body').css('background-image', `url("bar.jpg")`);
 	// }, 5000);
 	// $('body').css('background-image', 'none');
+	let a = "";
+	setInterval(function() {
+		// for(let i=0; i<3; i++) {
+		// console.log(String.fromCharCode(i));
+		// $('body').css('background-image', `url("https://s3-media2.fl.yelpcdn.com/bphoto/uOTbmDny6cardeqHlkc3LQ/o.jpg")`);
+		$('body').css('background', `url("https://s3-media3.fl.yelpcdn.com/bphoto/7w41a7ix8a4nH5CdGMBQLA/o.jpg") top left no-repeat, url("https://s3-media2.fl.yelpcdn.com/bphoto/uOTbmDny6cardeqHlkc3LQ/o.jpg") top right no-repeat`);
+		$('body').css('background-size', `50%, 50%`);
+	}, 5000);
+	// for(let i=97; i<1000; i++) {
+	// 	if(i%40 == 0) {
+	// 		console.log(a + "\t" + i);
+	// 		a = "";
+	// 	} else {
+	// 		a += String.fromCharCode(i);
+	// 	}
+	// }
 });
 
 function changeBackground() {
